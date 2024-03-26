178c1f55beee60365b3747342c55ea38ac3f6443
asymp <asymptotelian@gmail.com>
2024-03-26 17:56:58 +0200

    d02 working but have not passes by memory

---
+++ LB1/D-02/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	k := input()
+	fmt.Println(Solution(k))
+	//for i := 1; i <= 20; i++ {
+	//	println(Solution(i))
+	//}
+}
+
+func Solution(index int) int64 {
+	c := make([]int64, 0)
+	j := int64(1)
+	for i := int64(1); i <= int64(index) && len(c) < index; i++ {
+		cube := i * i * i
+		for ; j*j <= cube && len(c) < index; j++ {
+			if j*j == cube {
+				j++
+				break
+			}
+			c = append(c, j*j)
+		}
+		if len(c) < index {
+			c = append(c, cube)
+			//j++
+		} else {
+			break
+		}
+	}
+
+	return c[index-1]
+}
+
+func input() int {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	return n
+}
