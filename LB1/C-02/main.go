b8fe1b5eac185bf216987d8ba5c8fcb9204c1fff
Senior <senior@dev.lan>
2024-03-26 14:37:28 +0200

    LB1/C-02 but doesn't pass for some reason

---
+++ LB1/C-02/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	_, nums := input()
+	fmt.Println(Solution(nums))
+}
+
+func Solution(a []string) string {
+	for i := 1; i < len(a); i++ {
+		if digitSum(a[i-1]) >= digitSum(a[i]) {
+			for j := i; j > 0; j-- {
+				if digitSum(a[j-1]) > digitSum(a[j]) || (digitSum(a[j-1]) == digitSum(a[j]) && toInt(a[j-1]) > toInt(a[j])) {
+					a[j], a[j-1] = a[j-1], a[j]
+				} else {
+					break
+				}
+			}
+		}
+	}
+	return strings.Join(a, " ")
+}
+
+func toInt(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func digitSum(s string) int {
+	sum := 0
+	sign := ""
+	for _, ch := range s {
+		if ch == '-' {
+			sign = "-"
+			continue
+		}
+		digit, err := strconv.Atoi(sign + string(ch))
+		if err != nil {
+			panic(err)
+		}
+		sum += digit
+		sign = ""
+	}
+	return sum
+}
+
+func input() (int, []string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	var nums []string
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		nums = append(nums, scanner.Text())
+	}
+	return n, nums
+}
---
+++ LB1/C-02/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var inputs = []struct {
+	n      int
+	nums   []string
+	result string
+}{
+	{3, []string{"123", "-129", "221"}, "221 123 -129"},
+	{4, []string{"21", "3", "81", "27"}, "3 21 27 81"},
+}
+
+func TestSolution(t *testing.T) {
+	for _, in := range inputs {
+		assert.Equal(t, in.result, Solution(in.nums))
+	}
+}
