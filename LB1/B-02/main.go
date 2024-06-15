4040d32bd9ce1cd40021787e828af1baa6af2450
asymp <asymptotelian@gmail.com>
2024-06-15 11:32:51 +0300

    Revert "docs & comments"
    This reverts commit a75f6e49808fb9ed06ad828fa7e1274584f6f4e5.

--- LB1/B-02/main.go
+++ LB1/B-02/main.go
@@ -12,21 +12,11 @@ func main() {
 	fmt.Println(solution(nums, n))
 }
 
-// solution знаходить кількість різних чисел у заданому масиві.
-//
-// Аргументи:
-// a ([]int): Масив цілих чисел.
-// n (int): Кількість елементів у масиві.
-//
-// Повертає:
-// int: Кількість різних чисел у масиві.
 func solution(a []int, n int) int {
-	// Використовуємо map для зберігання унікальних елементів
 	elements := make(map[int]struct{})
 	for i := 0; i < n; i++ {
 		elements[a[i]] = struct{}{}
 	}
-	// Кількість унікальних елементів - це розмір map
 	return len(elements)
 }
 
