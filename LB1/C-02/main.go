d68742eb69182fa477e65b18b7287eade0a6ed50
Senior <senior@dev.lan>
2024-06-15 11:32:51 +0300

    Revert "docs & comments"
    This reverts commit a75f6e49808fb9ed06ad828fa7e1274584f6f4e5.

--- LB1/C-02/main.go
+++ LB1/C-02/main.go
@@ -26,20 +26,10 @@ func input() (int, []string) {
 	return n, nums
 }
 
-// solution сортує масив чисел у порядку неспадання суми їх цифр.
-// У разі рівності суми цифр, числа сортуються за значенням.
-// Використовується алгоритм сортування вставками. Складність - O(n^2).
-//
-// Аргументи:
-// a ([]string): Масив чисел у вигляді рядків.
-//
-// Повертає:
-// string: Відсортований масив чисел у вигляді одного рядка.
 func solution(a []string) string {
 	for i := 1; i < len(a); i++ {
 		if digitSum(a[i-1]) >= digitSum(a[i]) {
 			for j := i; j > 0; j-- {
-				// Порівняння за сумою цифр, а потім за значенням числа
 				if digitSum(a[j-1]) > digitSum(a[j]) || (digitSum(a[j-1]) == digitSum(a[j]) && toInt(a[j-1]) > toInt(a[j])) {
 					a[j], a[j-1] = a[j-1], a[j]
 				} else {
@@ -51,25 +41,11 @@ func solution(a []string) string {
 	return strings.Join(a, " ")
 }
 
-// toInt перетворює рядок у ціле число.
-//
-// Аргументи:
-// s (string): Рядок, що містить ціле число.
-//
-// Повертає:
-// int: Ціле число, отримане з рядка.
 func toInt(s string) int {
 	n, _ := strconv.Atoi(s)
 	return n
 }
 
-// digitSum обчислює суму цифр числа, представленого у вигляді рядка.
-//
-// Аргументи:
-// s (string): Рядок, що містить число.
-//
-// Повертає:
-// int: Сума цифр числа.
 func digitSum(s string) int {
 	sum := 0
 	for _, ch := range s {
